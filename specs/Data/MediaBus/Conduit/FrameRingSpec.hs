29e716507e2481a598c7efe3768d64e31bbf11c0
Sven Heyll <sven.heyll@lindenbaum.eu>
2020-05-09 05:36:28 +0200

    WIP

--- specs/Data/MediaBus/Conduit/FrameRingSpec.hs
+++ specs/Data/MediaBus/Conduit/FrameRingSpec.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Data.MediaBus.Conduit.FrameRingSpec (spec) where
 
 import Conduit
@@ -9,6 +13,29 @@ import Data.Word
 import Debug.Trace
 import Test.Hspec
 import Test.QuickCheck
+
+
+-- Behaviour:
+
+--   If the ratio of the duration between frame arrivals and the duration
+--   (i.e. /how many times longer it takes to receive a frame than to play a frame duration/)
+--   is > 1, then the ring will likely undeflow, otherwise, the ring will overflow.
+--
+--   For example, of the time between frame arrival is 25ms and each frame has a duration of 20ms,
+--   then it takes 1.2 times as long to receive than to play a frame.
+--
+--   A full frame is lost every ? frames.
+--     frameDuration/dt_arrive
+--   In order to reduce fragmentation as much as possible, the ring polling thread would need to
+--   delay for 1 frame (where it would send 1 frame of silence) and could then send 4
+--
+--   If no new frame was received after waiting for the duration of one packet, put a 'Missing' into the queue.
+--   If a frame arrives, put it into the ring, ignore the timestamp and sequence number.
+--   The stream must be reordered before being passed to the FrameRing.
+--   If a start frame is received, pass it through.
+--   When an overflow occurs with a frame that is not a start-frame, and the oldest frame is a start-frame,
+--   the second oldest frame will be removed.
+--   An overflow will  push out a start-frame, unless it is itself a start-frame.
 
 spec :: Spec
 spec =
--- src/Data/MediaBus/Conduit/FrameRing.hs
+++ src/Data/MediaBus/Conduit/FrameRing.hs
@@ -38,6 +38,7 @@ import Data.MediaBus.Media.Discontinous
 import Data.MediaBus.Media.SyncStream
 import Data.MediaBus.Media.Stream
 import Data.Proxy
+import Data.Semigroup
 import Data.String
 import Data.Time.Clock
 import Numeric.Natural
@@ -79,11 +80,15 @@ data Event c =
   | Underflow
     -- ^ The ring was empty for too long.
 
+
+
 -- | Create a new 'FrameRing' with an upper bound on the queue length.
 mkFrameRing ::
   (MonadIO m) =>
   Natural ->
   -- ^ Ring Element Count
+  Natural ->
+  -- ^ Minimum number of elements before pushing out
   m (FrameRing a)
 mkFrameRing qlen =
   MkFrameRing <$> newTBQueueIO qlen
@@ -95,10 +100,10 @@ frameRingSink ::
   (NFData sourceId, NFData streamStartPayload, NFData payload, MonadIO m) =>
   FrameRing (SyncStream sourceId streamStartPayload payload) ->
   ConduitT (SyncStream sourceId streamStartPayload payload) Void m ()
-frameRingSink (MkFrameRing !ringRef) = awaitForever pushInRing
+frameRingSink (MkFrameRing ringRef) = awaitForever pushInRing
   where
     pushInRing !buf' = do
-      !buf <- evaluate (withStrategy rdeepseq buf')
+      buf <- evaluate (withStrategy rdeepseq buf')
       atomically (do
         isFull <- isFullTBQueue ringRef
         overflow <-
@@ -106,23 +111,46 @@ frameRingSink (MkFrameRing !ringRef) = awaitForever pushInRing
             SinkOverflow . Just . snd
                 <$> readTBQueue ringRef
           else return (SinkOverflow Nothing)
-
         writeTBQueue ringRef (overflow, buf))
 
 
 -- | Internal helper.
 data SourceState = MkSourceState
 
+type FrameRingSourceStream sourceId streamStartPayload payload =
+    SyncStream
+      sourceId
+      streamStartPayload
+      (Discontinous
+         (Arg payload (SinkOverflow payload)))
+
+
 -- | Periodically poll a 'FrameRing' and yield the 'Frame's
--- put into the ring, or 'Missing' otherwise.
+--   put into the ring, or 'Missing' otherwise.
 --
--- Pass frames through.
--- If a start frame is received, pass it through.
--- If no frames are received for
-frameRingSource :: forall m sourceId streamStartPayload payload.
+-- Behaviour:
+--   The time between the arrival of each incoming frame is measured.
+--   When the time is greater than the frameDuration, the number of frames to queue before sending is enlarged.
+--   The ration of duration between frame reception and the duration of each frame is < 1 then the ring will likely overflow,
+--   otherwise, the ring will underflow.
+--   For example, of the time between frame arrival is 25ms and each frame has a duration of 20ms,
+--   the time to receive 4 frames is the time that it takes to play 5 frames.
+--   In order to reduce fragmentation as much as possible, the ring polling thread would need to
+--   delay for 1 frame (where it would send 1 frame of silence) and could then send 4
+--
+--   If no new frame was received after waiting for the duration of one packet, put a 'Missing' into the queue.
+--   If a frame arrives, put it into the ring, ignore the timestamp and sequence number.
+--   The stream must be reordered before being passed to the FrameRing.
+--   If a start frame is received, pass it through.
+--   When an overflow occurs with a frame that is not a start-frame, and the oldest frame is a start-frame,
+--   the second oldest frame will be removed.
+--   An overflow will  push out a start-frame, unless it is itself a start-frame.
+--
+-- @since 0.5.0.0
+frameRingSource :: forall m sourceId streamStartPayload payload .
   ( MonadIO m, HasStaticDuration payload ) =>
   FrameRing (SyncStream sourceId streamStartPayload payload) ->
-  ConduitT () (SyncStream sourceId streamStartPayload (SinkOverflow  payload)) m ()
+  ConduitT () (SyncStream sourceId streamStartPayload (SinkOverflow payload)) m ()
   --  TODO ConduitT () (Stream i s (Ticks r t) p (AnnotatedFrame (Maybe FrameRingEvent) (Discontinous c))) m ()
 frameRingSource  (MkFrameRing ringRef) =
   do
@@ -134,13 +162,6 @@ frameRingSource  (MkFrameRing ringRef) =
     pollIntervallMicros :: Ticks (Hz 1000000) Int
     pollIntervallMicros = nominalDiffTime # pTime
 
-    -- TODO this breaks when 'frameRingPollInterval < duration c'?
-    --      to fix add a 'timePassedSinceLastBufferReceived' parameter to 'go'
-    --      when no new from could be read from the queue after waiting for 'dt'
-    --      seconds, the time waited is added to 'frameRingPollInterval'
-    --      and if 'frameRingPollIntervall' is greater than the 'duration of c'
-    --      a 'Missing' is yielded and 'duration of c' is subtracted from
-    --      'timePassedSinceLastBufferReceived'.
     go = do
       res <- liftIO $ race (atomically $ readTBQueue ringRef) sleep
       case res of
