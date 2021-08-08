4273564219464f41e24d2ce776e529ac3709a914
Sven Heyll <sven.heyll@gmail.com>
2021-08-09 00:19:25 +0200

    Add missing file

---
+++ specs/FakePayload.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE NumericUnderscores #-}
+module FakePayload where
+
+import Data.MediaBus
+import Test.QuickCheck
+import Control.Lens
+import Data.Default
+import Control.DeepSeq
+
+newtype FakePayload
+  = FP
+      { fakeDuration :: Ticks64At8000
+      }
+  deriving (Eq, Show, Num, Arbitrary, Ord, NFData)
+
+instance Default FakePayload where
+  def = FP (MkTicks 60)
+
+instance HasDuration FakePayload where
+  getDuration = view (to fakeDuration . nominalDiffTime)
+
