5832ebf35b4bab1667db30afb930b7e91fa544ce
Sven Heyll <sven.heyll@gmail.com>
2021-08-27 06:00:23 +0200

    Cleanup Stream code

--- specs/Data/MediaBus/Conduit/StreamSpec.hs
+++ specs/Data/MediaBus/Conduit/StreamSpec.hs
@@ -2,81 +2,48 @@ module Data.MediaBus.Conduit.StreamSpec (spec) where
 
 import Conduit
 import Control.Lens
-import Control.Monad
+import Control.Monad.Logger
 import Data.Conduit.List
 import Data.MediaBus
-import Data.Word
 import Test.Hspec
 import Test.QuickCheck
 
 spec :: Spec
-spec =
-  describe "Stream conduits"
-    $ do
-        describe "Functor instance: fmap"
-              $ it "should have the same result as 'overFrameContentC (...) f'" pending
-        describe "assumeSynchronized" $
-          it "removes sequence numbers and timestamps but passes all buffers untouched" $
-            property $ \(inputs :: [Stream Bool Int Int Int Int]) ->
-              let outputs = runConduitPure (yieldMany inputs .| assumeSynchronizedC .| sinkList )
-              in
-                length inputs === length outputs
-                .&&.
-                toListOf (each . eachFramePayload) inputs
-                 === toListOf (each . eachFramePayload) outputs
+spec = do
+  describe "Stream functions" $
+    do
+      describe "isStartFrame" $
+        it "returns True for start frames and False otherwise" $  do
+          isStartFrame (MkStream (Start (MkFrameCtx () () () ()))) `shouldBe` True
+          isStartFrame (MkStream (Next (MkFrame () () ()))) `shouldBe` False
+      describe "isPayloadFrame" $
+        it "returns True for payload frames and False otherwise" $  do
+          isPayloadFrame (MkStream (Start (MkFrameCtx () () () ()))) `shouldBe` False
+          isPayloadFrame (MkStream (Next (MkFrame () () ()))) `shouldBe` True
 
--- fmap f = overFrameContentC undefined (mapInput _framePayload (const Nothing) (const f))
-_helloWorld :: IO ()
-_helloWorld =
-  void $
-    runConduit
-      ( yieldMany ("Hello world" :: String)
-          .| traceShowC 1 "YO"
-          .| consume
-      )
-
-_sampleSomeStream :: IO ALittleOutOfOrder
-_sampleSomeStream = MkALittleOutOfOrder <$> sample' arbitrary
-
-_yieldStream ::
-  Monad m =>
-  ALittleOutOfOrder ->
-  ConduitT () (Stream () (SeqNum Word16) () () ()) m ()
-_yieldStream (MkALittleOutOfOrder someFrames) =
-  yieldMany someFrames
-    .| traceShowC 1 "ORIGINAL"
-
-_reorderSomeFrames =
-  void $
-    _sampleSomeStream
-      >>= ( \fs ->
-              runConduit
-                ( _yieldStream fs
-                    .| reorderFramesBySeqNumC 2
-                    .| traceShowC 1 "     ORDERED"
-                    .| consume
-                )
-          )
-
-newtype ALittleOutOfOrder
-  = MkALittleOutOfOrder
-      { fromALittleOutOfOrder :: [Stream () (SeqNum Word16) () () ()]
-      }
-
-instance Arbitrary ALittleOutOfOrder where
-  arbitrary = do
-    start <- MkStream . Start <$> arbitrary
-    (len :: Int) <- arbitrary
-    MkALittleOutOfOrder <$> loop (start ^. seqNum) (len + 10) [start]
-    where
-      loop (MkSeqNum lastSeq) n acc
-        | n == 0 = return acc
-        | otherwise = do
-          nextSeq' <- choose (lastSeq + 1, lastSeq + 2)
-          ts <- arbitrary
-          c <- arbitrary
-          let nextSeq = MkSeqNum nextSeq'
-          loop
-            nextSeq
-            (n - 1)
-            (MkStream (Next (MkFrame ts nextSeq c)) : acc)
+  describe "Stream conduits" $
+    do
+      describe "logStreamC" $ do
+        it "passes through the values when no logging is performced" $
+          property $ \(inputs :: [Stream Bool Int Int Int Int]) -> ioProperty $ do
+            outputs <-
+              runNoLoggingT
+                . runResourceT
+                . runConduit
+                $ (yieldMany inputs .| logStreamC (const Nothing) "test" .| consume)
+            return (inputs === outputs)
+        it "passes through the values when logging is performced" $
+          property $ \(inputs :: [Stream Bool Int Int Int Int]) -> ioProperty $ do
+            outputs <-
+              runNoLoggingT
+                . runResourceT
+                . runConduit
+                $ (yieldMany inputs .| logStreamC (const (Just LevelWarn)) "test" .| consume)
+            return (inputs === outputs)
+      describe "assumeSynchronized" $
+        it "removes sequence numbers and timestamps but passes all buffers untouched" $
+          property $ \(inputs :: [Stream Bool Int Int Int Int]) ->
+            let outputs = runConduitPure (yieldMany inputs .| assumeSynchronizedC .| sinkList)
+             in length inputs === length outputs
+                  .&&. toListOf (each . eachFramePayload) inputs
+                    === toListOf (each . eachFramePayload) outputs
